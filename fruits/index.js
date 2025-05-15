d2bc000b43d6bafcf2b9f0c0c4a01b75a3893a18
codewithghouse <personalghouse@gmail.com>
2025-05-15 09:49:09 +0530

    semiolod added

--- fruits/index.js
+++ fruits/index.js
@@ -1,6 +1,6 @@
 // files ke data ku require karke export karre dusre folder je file me 
 let apple = require("./apple");
-let banana = require("./banana")
+let banana = require("./banana");
 let mango = require("./mango");
 
 let fruitsdetails = [ apple , banana , mango];
