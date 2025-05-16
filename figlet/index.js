531d1441758e5308a205b6736518d92701ee9751
codewithghouse <personalghouse@gmail.com>
2025-05-16 10:32:42 +0530

    commet added

--- figlet/index.js
+++ figlet/index.js
@@ -1,4 +1,6 @@
-const figlet= require("figlet");
+const figlet= require("figlet"); // this is package name 
+
+// below code is for accessing the package 
 figlet("Ghouse", function (err, data) {
   if (err) {
     console.log("Something went wrong...");
